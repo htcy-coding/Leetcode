cbc8ac8817df6d52f33352d0a7e2aa0f27736193
yuyang7 <yuyang7@shein.com>
2022-11-24 20:51:52 +0800

    Initial commit

---
+++ src/lianbiao/Fanzhuanlianbiao.java
@@ -0,0 +1,51 @@
+package lianbiao;
+
+import java.util.List;
+
+//翻转链表
+public class Fanzhuanlianbiao {
+
+    public static void main(String[] args) {
+
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode12 = new ListNode(2);
+        ListNode listNode13 = new ListNode(3);
+        ListNode listNode14 = new ListNode(4);
+        listNode1.next = listNode12;
+        listNode12.next = listNode13;
+        listNode13.next = listNode14;
+
+        ListNode ams = fanzhuan1(listNode1);
+//        ListNode t = ams;
+        while (ams != null) {
+            System.out.println(ams.value);
+            ams = ams.next;
+        }
+
+    }
+
+    //迭代
+   static ListNode fanzhuan(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    //递归
+    static ListNode fanzhuan1(ListNode head){
+       if (head == null || head.next == null) {return head;}
+       ListNode listNode = fanzhuan1(head.next);
+       head.next.next = head;
+       head.next = null;
+       return listNode;
+    }
+
+
+
+}
---
+++ src/lianbiao/ListNode.java
@@ -0,0 +1,13 @@
+package lianbiao;
+
+import java.util.List;
+
+public class ListNode {
+    ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+
+    int value;
+}
